4153e6520b51f25f9baa3051e56bc9fde2b04c62
şira eroğlu <siraeroglu@hotmail.com>
2026-03-26 12:44:53 +0300

    prepare github pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom"
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom"
 import { useEffect, lazy, Suspense } from "react"
 import { HelmetProvider } from "react-helmet-async"
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/etik-hastanesi/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
